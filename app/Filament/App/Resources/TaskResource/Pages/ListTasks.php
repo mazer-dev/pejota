6b3a03baf2f32df3d6bbdde7d787bd83dabf317b
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2024-05-24 08:53:48 -0300

    feat: tabs opened and closed

--- app/Filament/App/Resources/TaskResource/Pages/ListTasks.php
+++ app/Filament/App/Resources/TaskResource/Pages/ListTasks.php
@@ -2,9 +2,13 @@
 
 namespace App\Filament\App\Resources\TaskResource\Pages;
 
+use App\Enums\PhaseEnum;
+use App\Enums\StatusPhaseEnum;
 use App\Filament\App\Resources\TaskResource;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListTasks extends ListRecords
 {
@@ -14,6 +18,28 @@ class ListTasks extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            'all' => Tab::make(),
+            'opened' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query): Builder
+                    => $query->whereHas('status', function (Builder $query) {
+                        $query->whereIn('phase', [
+                            StatusPhaseEnum::TODO,
+                            StatusPhaseEnum::IN_PROGRESS,
+                        ]);
+                    })),
+            'closed' => Tab::make()
+                ->modifyQueryUsing(fn (Builder $query): Builder
+                    => $query->whereHas('status', function (Builder $query) {
+                        $query->whereIn('phase', [
+                            StatusPhaseEnum::CLOSED,
+                        ]);
+                    })),
         ];
     }
 }
