599d7df6d55ac310f5832e6b66844d83a11c365d
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2024-05-25 15:58:57 -0300

    feat: better comments on client view

--- app/Filament/App/Resources/ClientResource.php
+++ app/Filament/App/Resources/ClientResource.php
@@ -5,15 +5,28 @@ namespace App\Filament\App\Resources;
 use App\Filament\App\Resources\ClientResource\Pages;
 use App\Filament\App\Resources\ClientResource\RelationManagers;
 use App\Models\Client;
+use Filament\Actions\ViewAction;
 use Filament\Forms;
 use Filament\Forms\Form;
+use Filament\Infolists\Components\Actions;
+use Filament\Infolists\Components\Actions\Action;
+use Filament\Infolists\Components\Grid;
+use Filament\Infolists\Components\Livewire;
+use Filament\Infolists\Components\Section;
+use Filament\Infolists\Components\Split;
+use Filament\Infolists\Components\TextEntry;
 use Filament\Infolists\Infolist;
 use Filament\Resources\Resource;
+use Filament\Support\Enums\FontWeight;
 use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\HtmlString;
 use Parallax\FilamentComments\Infolists\Components\CommentsEntry;
+use Parallax\FilamentComments\Models\FilamentComment;
+use Parallax\FilamentComments\Tables\Actions\CommentsAction;
 
 class ClientResource extends Resource
 {
@@ -30,8 +43,7 @@ class ClientResource extends Resource
                     ->required(),
                 Forms\Components\TextInput::make('tradename'),
                 Forms\Components\TextInput::make('email')
-                    ->email()
-                    ->required(),
+                    ->email(),
                 Forms\Components\TextInput::make('phone')
                     ->tel(),
             ]);
@@ -40,6 +52,7 @@ class ClientResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
+            ->striped(true)
             ->columns([
                 Tables\Columns\TextColumn::make('name')
                     ->searchable(),
@@ -62,6 +75,10 @@ class ClientResource extends Resource
                 //
             ])
             ->actions([
+                Tables\Actions\ViewAction::make()
+                    ->iconButton(),
+                CommentsAction::make()
+                    ->iconButton(),
                 Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
@@ -71,6 +88,61 @@ class ClientResource extends Resource
             ]);
     }
 
+    public static function infolist(Infolist $infolist): Infolist
+    {
+        return $infolist
+            ->schema([
+                Split::make([
+                    Grid::make(1)
+                        ->schema([
+                            Section::make([
+                                TextEntry::make('name')
+                                    ->size(TextEntry\TextEntrySize::Large)
+                                    ->weight(FontWeight::Bold)
+                                    ->label(''),
+
+                                TextEntry::make('trademark')
+                                    ->size(TextEntry\TextEntrySize::Large)
+                                    ->label('')
+                                    ->icon('heroicon-o-at-bookmark-square'),
+
+                                TextEntry::make('email')
+                                    ->label('')
+                                    ->icon('heroicon-o-at-symbol'),
+
+                                TextEntry::make('phone')
+                                    ->label('')
+                                    ->icon('heroicon-o-phone')
+
+                            ]),
+
+                            Section::make('Comments')
+                                ->collapsible()
+                                ->schema([
+                                    CommentsEntry::make('fialament_comments')
+                                        ->columnSpanFull(),
+                                ]),
+                        ]),
+
+
+                    Section::make([
+                        TextEntry::make('created_at')
+                            ->dateTime(),
+                        TextEntry::make('updated_at')
+                            ->dateTime(),
+                        Actions::make([
+                            Action::make('edit')
+                                ->url(
+                                    fn(Model $record) => "{$record->id}/edit"
+                                )
+                        ])
+                    ])->grow(false),
+
+                ])
+                    ->columnSpanFull(),
+
+            ]);
+    }
 
     public static function getRelations(): array
     {
@@ -84,6 +156,7 @@ class ClientResource extends Resource
         return [
             'index' => Pages\ListClients::route('/'),
             'create' => Pages\CreateClient::route('/create'),
+            'view' => Pages\ViewClient::route('/{record}'),
             'edit' => Pages\EditClient::route('/{record}/edit'),
         ];
     }
--- database/migrations/2024_05_21_003805_create_clients_table.php
+++ database/migrations/2024_05_21_003805_create_clients_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
 
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->nullable();
             $table->string('phone')->nullable();
 
             $table->unsignedInteger('company_id');
