b61542f2a7f07f8a89939af7ac2d833c95c8dbfa
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2024-05-28 18:36:45 -0300

    feat: belonts to tenant

--- app/Models/WorkSession.php
+++ app/Models/WorkSession.php
@@ -4,8 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use NunoMazer\Samehouse\BelongsToTenants;
 
 class WorkSession extends Model
 {
-    use HasFactory;
+    use HasFactory,
+        BelongsToTenants;
 }
