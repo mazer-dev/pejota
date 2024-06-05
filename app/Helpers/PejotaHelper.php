57ec704ddb82ebe1bd93da796e17b3dc9b1b5951
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2024-06-04 23:17:58 -0300

    refact: improve duraton formatter

--- app/Helpers/PejotaHelper.php
+++ app/Helpers/PejotaHelper.php
@@ -12,15 +12,15 @@ class PejotaHelper
      * @param int $duration
      * @return string
      */
-    public static function formatDuration(?int $duration): string
+    public static function formatDuration(?int $duration, bool $formatWithDots = false): string
     {
         if (!$duration) {
-            return '00:00';
+            return $formatWithDots ? '00:00' : '00h00';
         }
 
         return
             str_pad(intdiv($duration, 60), 2, '0', STR_PAD_LEFT)
-            .':'.
+            . ( $formatWithDots ? ':' : 'h') .
             str_pad($duration % 60, 2, '0', STR_PAD_LEFT);
     }
 
