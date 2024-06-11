763a0a6486ca3d4560503a3ad3a983e9f00c0d76
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2024-06-10 22:21:52 -0300

    feat: some icons

--- app/Filament/App/Widgets/OverallNumbersOverview.php
+++ app/Filament/App/Widgets/OverallNumbersOverview.php
@@ -3,18 +3,26 @@
 namespace App\Filament\App\Widgets;
 
 use App\Enums\StatusPhaseEnum;
+use App\Filament\App\Resources\ClientResource;
+use App\Filament\App\Resources\ProjectResource;
+use App\Filament\App\Resources\TaskResource;
 use App\Models\Client;
 use App\Models\Task;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use App\Models\Project;
+
 class OverallNumbersOverview extends BaseWidget
 {
     protected function getStats(): array
     {
         return [
-            Stat::make('Clients', Client::count()),
-            Stat::make('Projects', Project::count()),
+            Stat::make('Clients', Client::count())
+                ->icon(ClientResource::getNavigationIcon()),
+
+            Stat::make('Projects', Project::count())
+                ->icon(ProjectResource::getNavigationIcon()),
+
             Stat::make('Tasks Opened',
                 Task::whereHas('status', function ($query) {
                     return $query->whereIn('phase', [
@@ -22,7 +30,8 @@ class OverallNumbersOverview extends BaseWidget
                         StatusPhaseEnum::IN_PROGRESS
                     ]);
                 })->count()
-            ),
+            )
+                ->icon(TaskResource::getNavigationIcon()),
         ];
     }
 }
