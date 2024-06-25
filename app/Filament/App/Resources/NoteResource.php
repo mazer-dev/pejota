03106fa00809ddbe14ef422e1b1eb0e7ffcd4d50
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2024-06-24 23:49:35 -0300

    feat: markdown, richtext, code, plain text notes

--- app/Filament/App/Resources/NoteResource.php
+++ app/Filament/App/Resources/NoteResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\App\Resources;
 
+use AbdelhamidErrahmouni\FilamentMonacoEditor\MonacoEditor;
 use App\Filament\App\Resources\NoteResource\Pages;
 use App\Filament\App\Resources\NoteResource\RelationManagers;
 use App\Models\Note;
@@ -19,6 +20,8 @@ class NoteResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-pencil-square';
     protected static ?int $navigationSort = 1;
+
+
 
     public static function form(Form $form): Form
     {
@@ -50,7 +53,70 @@ class NoteResource extends Resource
                                 Forms\Components\TextInput::make('title')
                                     ->hiddenLabel()
                                     ->placeholder('Title'),
-                            ])
+                            ]),
+
+                        Forms\Components\Builder\Block::make('code')
+                            ->icon('heroicon-o-code-bracket')
+                            ->schema([
+                                Forms\Components\Select::make('language')
+                                    ->options([
+                                        'bash' => 'Bash',
+                                        'bat' => 'Batch',
+                                        'c' => 'C',
+                                        'csharp' => 'C#',
+                                        'css' => 'CSS',
+                                        'csv' => 'CSV',
+                                        'dart' => 'Dart',
+                                        'diff' => 'Diff',
+                                        'dockerfile' => 'Dockerfile',
+                                        'elixir' => 'Elixir',
+                                        'go' => 'Go',
+                                        'html' => 'HTML',
+                                        'ini' => 'INI',
+                                        'java' => 'Java',
+                                        'javascript' => 'JavaScript',
+                                        'json' => 'JSON',
+                                        'kotlin' => 'Kotlin',
+                                        'less' => 'Less',
+                                        'lua' => 'Lua',
+                                        'markdown' => 'Markdown',
+                                        'perl' => 'Perl',
+                                        'php' => 'PHP',
+                                        'python' => 'Python',
+                                        'redis' => 'Redis',
+                                        'ruby' => 'Ruby',
+                                        'rust' => 'Rust',
+                                        'scss' => 'SCSS',
+                                        'sql' => 'SQL',
+                                        'swift' => 'Swift',
+                                        'typescript' => 'TypeScript',
+                                        'xml' => 'XML',
+                                        'yaml' => 'YAML',
+                                    ])
+                                    ->live(),
+
+                                MonacoEditor::make('content')
+                                    ->language(fn($get) => ($get('language') ?? 'html'))
+                                    ->disablePreview(true),
+                            ]),
+
+                        Forms\Components\Builder\Block::make('markdown')
+                            ->icon('heroicon-o-code-bracket-square')
+                            ->schema([
+                                Forms\Components\MarkdownEditor::make('content'),
+                            ]),
+
+                        Forms\Components\Builder\Block::make('richtext')
+                            ->icon('heroicon-o-code-bracket-square')
+                            ->schema([
+                                Forms\Components\RichEditor::make('content'),
+                            ]),
+
+                        Forms\Components\Builder\Block::make('text')
+                            ->icon('heroicon-o-document-text')
+                            ->schema([
+                                Forms\Components\Textarea::make('content'),
+                            ]),
                     ]),
             ]);
     }
