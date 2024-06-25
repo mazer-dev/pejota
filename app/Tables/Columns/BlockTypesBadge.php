4347494e50d0b990a2e32420476023027a77aa75
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2024-06-24 23:04:08 -0300

    feat: show types of content on table list

---
+++ app/Tables/Columns/BlockTypesBadge.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Tables\Columns;
+
+use Filament\Support\Concerns\HasColor;
+use Filament\Tables\Columns\Column;
+
+class BlockTypesBadge extends Column
+{
+    use HasColor;
+    protected string $view = 'tables.columns.block-types-badge';
+}
