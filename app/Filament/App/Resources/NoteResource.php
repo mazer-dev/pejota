0e3a6e66a32fdf9f879b65f108024a305e9425e7
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2024-06-30 20:19:12 -0300

    layout: better split layout

--- app/Filament/App/Resources/NoteResource.php
+++ app/Filament/App/Resources/NoteResource.php
@@ -22,107 +22,115 @@ class NoteResource extends Resource
     protected static ?int $navigationSort = 1;
 
 
-
     public static function form(Form $form): Form
     {
         return $form
             ->columns(1)
             ->schema([
-                Forms\Components\TextInput::make('title')
-                    ->required(),
-                Forms\Components\SpatieTagsInput::make('tags'),
+                Forms\Components\Split::make([
+                    Forms\Components\Section::make()->schema([
+                        Forms\Components\TextInput::make('title')
+                            ->required(),
 
-                Forms\Components\Builder::make('content')
-                    ->columnSpanFull()
-                    ->blocks([
-                        Forms\Components\Builder\Block::make('link')
-                            ->columns(2)
-                            ->inlineLabel()
-                            ->icon('heroicon-o-link')
-                            ->schema([
-                                Forms\Components\TextInput::make('url')
-                                    ->hiddenLabel()
-                                    ->placeholder('url')
-                                    ->required()
-                                    ->prefixAction(
-                                        fn ($state) => Forms\Components\Actions\Action::make('url')
-                                            ->url($state)
-                                            ->openUrlInNewTab()
-                                            ->icon('heroicon-o-link')
-                                    ),
-                                Forms\Components\TextInput::make('title')
-                                    ->hiddenLabel()
-                                    ->placeholder('Title'),
-                            ]),
+                        Forms\Components\Builder::make('content')
+                            ->columnSpanFull()
+                            ->blocks([
+                                Forms\Components\Builder\Block::make('link')
+                                    ->columns(2)
+                                    ->inlineLabel()
+                                    ->icon('heroicon-o-link')
+                                    ->schema([
+                                        Forms\Components\TextInput::make('url')
+                                            ->hiddenLabel()
+                                            ->placeholder('url')
+                                            ->required()
+                                            ->prefixAction(
+                                                fn($state) => Forms\Components\Actions\Action::make('url')
+                                                    ->url($state)
+                                                    ->openUrlInNewTab()
+                                                    ->icon('heroicon-o-link')
+                                            ),
+                                        Forms\Components\TextInput::make('title')
+                                            ->hiddenLabel()
+                                            ->placeholder('Title'),
+                                    ]),
 
-                        Forms\Components\Builder\Block::make('code')
-                            ->icon('heroicon-o-code-bracket')
-                            ->schema([
-                                Forms\Components\Select::make('language')
-                                    ->options([
-                                        'bash' => 'Bash',
-                                        'bat' => 'Batch',
-                                        'c' => 'C',
-                                        'csharp' => 'C#',
-                                        'css' => 'CSS',
-                                        'csv' => 'CSV',
-                                        'dart' => 'Dart',
-                                        'diff' => 'Diff',
-                                        'dockerfile' => 'Dockerfile',
-                                        'elixir' => 'Elixir',
-                                        'go' => 'Go',
-                                        'html' => 'HTML',
-                                        'ini' => 'INI',
-                                        'java' => 'Java',
-                                        'javascript' => 'JavaScript',
-                                        'json' => 'JSON',
-                                        'kotlin' => 'Kotlin',
-                                        'less' => 'Less',
-                                        'lua' => 'Lua',
-                                        'markdown' => 'Markdown',
-                                        'perl' => 'Perl',
-                                        'php' => 'PHP',
-                                        'python' => 'Python',
-                                        'redis' => 'Redis',
-                                        'ruby' => 'Ruby',
-                                        'rust' => 'Rust',
-                                        'scss' => 'SCSS',
-                                        'sql' => 'SQL',
-                                        'swift' => 'Swift',
-                                        'typescript' => 'TypeScript',
-                                        'xml' => 'XML',
-                                        'yaml' => 'YAML',
-                                    ])
-                                    ->live(),
+                                Forms\Components\Builder\Block::make('code')
+                                    ->icon('heroicon-o-code-bracket')
+                                    ->schema([
+                                        Forms\Components\Select::make('language')
+                                            ->options([
+                                                'bash' => 'Bash',
+                                                'bat' => 'Batch',
+                                                'c' => 'C',
+                                                'csharp' => 'C#',
+                                                'css' => 'CSS',
+                                                'csv' => 'CSV',
+                                                'dart' => 'Dart',
+                                                'diff' => 'Diff',
+                                                'dockerfile' => 'Dockerfile',
+                                                'elixir' => 'Elixir',
+                                                'go' => 'Go',
+                                                'html' => 'HTML',
+                                                'ini' => 'INI',
+                                                'java' => 'Java',
+                                                'javascript' => 'JavaScript',
+                                                'json' => 'JSON',
+                                                'kotlin' => 'Kotlin',
+                                                'less' => 'Less',
+                                                'lua' => 'Lua',
+                                                'markdown' => 'Markdown',
+                                                'perl' => 'Perl',
+                                                'php' => 'PHP',
+                                                'python' => 'Python',
+                                                'redis' => 'Redis',
+                                                'ruby' => 'Ruby',
+                                                'rust' => 'Rust',
+                                                'scss' => 'SCSS',
+                                                'sql' => 'SQL',
+                                                'swift' => 'Swift',
+                                                'typescript' => 'TypeScript',
+                                                'xml' => 'XML',
+                                                'yaml' => 'YAML',
+                                            ])
+                                            ->live(),
 
-                                MonacoEditor::make('content')
-                                    ->language(fn($get) => ($get('language') ?? 'html'))
-                                    ->disablePreview(true)
-                                    ->hideFullScreenButton()
-                                    ->hiddenLabel(),
-                            ]),
+                                        MonacoEditor::make('content')
+                                            ->language(fn($get) => ($get('language') ?? 'html'))
+                                            ->disablePreview(true)
+                                            ->hideFullScreenButton()
+                                            ->hiddenLabel(),
+                                    ]),
 
-                        Forms\Components\Builder\Block::make('markdown')
-                            ->icon('heroicon-o-code-bracket-square')
-                            ->schema([
-                                Forms\Components\MarkdownEditor::make('content')
-                                    ->hiddenLabel(),
-                            ]),
+                                Forms\Components\Builder\Block::make('markdown')
+                                    ->icon('heroicon-o-code-bracket-square')
+                                    ->schema([
+                                        Forms\Components\MarkdownEditor::make('content')
+                                            ->hiddenLabel(),
+                                    ]),
 
-                        Forms\Components\Builder\Block::make('richtext')
-                            ->icon('heroicon-o-code-bracket-square')
-                            ->schema([
-                                Forms\Components\RichEditor::make('content')
-                                    ->hiddenLabel(),
-                            ]),
+                                Forms\Components\Builder\Block::make('richtext')
+                                    ->icon('heroicon-o-code-bracket-square')
+                                    ->schema([
+                                        Forms\Components\RichEditor::make('content')
+                                            ->hiddenLabel(),
+                                    ]),
 
-                        Forms\Components\Builder\Block::make('text')
-                            ->icon('heroicon-o-document-text')
-                            ->schema([
-                                Forms\Components\Textarea::make('content')
-                                    ->hiddenLabel(),
+                                Forms\Components\Builder\Block::make('text')
+                                    ->icon('heroicon-o-document-text')
+                                    ->schema([
+                                        Forms\Components\Textarea::make('content')
+                                            ->hiddenLabel(),
+                                    ]),
                             ]),
                     ]),
+
+                    Forms\Components\Section::make()->schema([
+                        Forms\Components\SpatieTagsInput::make('tags'),
+
+                    ])
+                        ->grow(false)
+                ]),
             ]);
     }
 
