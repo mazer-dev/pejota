e7caf3b841120bcfc623d7e2d7d218e4153b87c5
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2024-07-12 22:16:50 -0300

    feat(#30): vendor module

--- app/Enums/MenuSortEnum.php
+++ app/Enums/MenuSortEnum.php
@@ -5,6 +5,7 @@ namespace App\Enums;
 enum MenuSortEnum: int
 {
     case CLIENTS = 0;
+    case VENDORS = 5;
     case PROJECTS = 10;
     case CONTRACTS = 20;
     case TASKS = 30;
---
+++ database/migrations/2024_07_12_220805_create_vendors_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name');
+            $table->string('tradename')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone')->nullable();
+            $table->foreignId('company_id')
+                ->constrained('companies')
+                ->restrictOnDelete();
+
+            $table->timestamps();
+        });
+
+        Schema::table('contracts', function (Blueprint $table) {
+            $table->foreignIdFor(\App\Models\Vendor::class, 'vendor_id')
+                ->nullable();
+
+            $table->foreignIdFor(\App\Models\Client::class, 'client_id')
+                ->nullable()
+                ->change();
+
+            $table->decimal('total', 12, 2)->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('vendors');
+    }
+};
