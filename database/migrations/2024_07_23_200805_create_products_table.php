1ac1190c56067d3d9c1762f6c041f3293362db0c
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2024-07-23 21:17:11 -0300

    fix: products table creation

--- database/migrations/2024_07_23_200805_create_products_table.php
+++ database/migrations/2024_07_23_200805_create_products_table.php
@@ -23,7 +23,7 @@ return new class extends Migration {
             $table->timestamps();
         });
 
-        Schema::table('products', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
 
             $table->string('name');
