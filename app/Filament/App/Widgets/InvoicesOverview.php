7c30f4132bc2b3dac57923aa41953bbefb2be072
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2024-09-14 18:50:57 -0300

    feat: more invoices widgets

--- app/Filament/App/Widgets/InvoicesOverview.php
+++ app/Filament/App/Widgets/InvoicesOverview.php
@@ -18,17 +18,39 @@ class InvoicesOverview extends BaseWidget
         $today = CarbonImmutable::now(PejotaHelper::getUserTimeZone())->startOfDay();
 
         // TODO improve and centralize the money handling
-        $total = Invoice::where('status', InvoiceStatusEnum::SENT->value)
+        $totalOpened = Invoice::where('status', InvoiceStatusEnum::SENT->value)
+                    ->sum('total') / 100;
+
+        $totalOverdue = Invoice::where('status', InvoiceStatusEnum::SENT->value)
+                    ->where('due_date', '<', $today)
+                    ->sum('total') / 100;
+
+        $totalPaid = Invoice::where('status', InvoiceStatusEnum::PAID->value)
+                    ->where('payment_date', '>=', $today->startOfMonth())
                     ->sum('total') / 100;
         $fmt = \NumberFormatter::create(PejotaHelper::getUserLocate(), \NumberFormatter::CURRENCY);
 
         return [
             Stat::make(__('Invoices Opened'),
-                $fmt->formatCurrency($total, PejotaHelper::getUserCurrency())
+                $fmt->formatCurrency($totalOpened, PejotaHelper::getUserCurrency())
             )
                 ->description(__('Invoices with status SENT'))
                 ->icon('heroicon-o-currency-dollar')
                 ->color(Color::hex('#dadada')),
+
+            Stat::make(__('Invoices Overdue'),
+                $fmt->formatCurrency($totalOverdue, PejotaHelper::getUserCurrency())
+            )
+                ->description(__('Invoices SENT and overdue'))
+                ->icon('heroicon-o-currency-dollar')
+                ->color(Color::hex('#fadada')),
+
+            Stat::make(__('Received this month'),
+                $fmt->formatCurrency($totalPaid, PejotaHelper::getUserCurrency())
+            )
+                ->description(__('Invoices PAID this month'))
+                ->icon('heroicon-o-currency-dollar')
+                ->color(Color::hex('#daFada')),
         ];
     }
 }
