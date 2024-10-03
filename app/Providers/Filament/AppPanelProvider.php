4114d447f6c934171eab54a2ad7a6744816f42af
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2024-10-02 21:02:55 -0300

    layout: striped tables

--- app/Providers/Filament/AppPanelProvider.php
+++ app/Providers/Filament/AppPanelProvider.php
@@ -39,7 +39,6 @@ class AppPanelProvider extends PanelProvider
             ->favicon(asset('imgs/pejota-logo.svg'))
             ->colors([
                 'primary' => Color::hex('#00BF63'),
-                'gray' => Color::hex('#506050'),
             ])
             ->viteTheme('resources/css/filament/app/theme.css')
             ->sidebarWidth('15rem;')
