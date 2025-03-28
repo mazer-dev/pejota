e2f7620639b644845be623f85c5a8c7eae5a3c57
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2025-03-27 21:02:32 -0300

    fix: allow user to access panel on production

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Events\UserCreated;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
