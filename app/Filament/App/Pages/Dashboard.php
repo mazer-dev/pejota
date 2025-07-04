3c9a879b47c5f474a1e6999b7c2d6bdbbc3f89c8
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2025-07-03 21:54:35 -0300

    feat: coluns 4

--- app/Filament/App/Pages/Dashboard.php
+++ app/Filament/App/Pages/Dashboard.php
@@ -11,4 +11,9 @@ class Dashboard extends \Filament\Pages\Dashboard
 {
     protected static ?string $navigationIcon = 'heroicon-o-chart-pie';
 
+    public function getColumns(): int|string|array
+    {
+        return 6;
+    }
+
 }
