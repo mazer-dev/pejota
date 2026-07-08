694ced55a2e0a7865e4e8c565234d12cc2826b8c
luizhakan <luizhakan2@protonmail.com>
2026-07-08 12:23:04 -0300

    feat: adiciona caso NORMAL ao enum PriorityEnum e ajusta métodos relacionados

--- app/Enums/PriorityEnum.php
+++ app/Enums/PriorityEnum.php
@@ -11,6 +11,7 @@ enum PriorityEnum: string implements HasColor, HasIcon, HasLabel
 {
     case LOW = 'low';
     case MEDIUM = 'medium';
+    case NORMAL = 'normal';
     case HIGH = 'high';
     case URGENT = 'urgent';
     case CRITICAL = 'critical';
@@ -28,7 +29,7 @@ enum PriorityEnum: string implements HasColor, HasIcon, HasLabel
     {
         return match ($this) {
             self::LOW => 0,
-            self::MEDIUM => 1,
+            self::MEDIUM, self::NORMAL => 1,
             self::HIGH => 2,
             self::URGENT => 3,
             self::CRITICAL => 4,
@@ -41,7 +42,7 @@ enum PriorityEnum: string implements HasColor, HasIcon, HasLabel
             self::CRITICAL => 'heroicon-o-exclamation-circle',
             self::URGENT => 'heroicon-o-chevron-double-up',
             self::HIGH => 'heroicon-o-chevron-up',
-            self::MEDIUM => 'heroicon-o-minus',
+            self::MEDIUM, self::NORMAL => 'heroicon-o-minus',
             self::LOW => 'heroicon-o-chevron-down',
         };
     }
@@ -52,7 +53,7 @@ enum PriorityEnum: string implements HasColor, HasIcon, HasLabel
             self::CRITICAL => Color::Red,
             self::URGENT => Color::Orange,
             self::HIGH => Color::Yellow,
-            self::MEDIUM => Color::Blue,
+            self::MEDIUM, self::NORMAL => Color::Blue,
             self::LOW => Color::Neutral,
         };
     }
