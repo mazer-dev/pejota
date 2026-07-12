f37ea5b5f5e6ab007ce171c4d1cb7611ae13827b
Ademir Mazer Junior <ademir.mazer.jr@gmail.com>
2026-07-11 22:04:37 -0300

    feat: add UserSettingsEnum for managing user localization and timezone settings

---
+++ app/Enums/UserSettingsEnum.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Enums;
+
+use DateTime;
+use DateTimeZone;
+
+enum UserSettingsEnum: string
+{
+    case LOCALIZATION_LOCALE = 'localization.locale';
+    case LOCALIZATION_TIMEZONE = 'localization.timezone';
+    case LOCALIZATION_DATE_FORMAT = 'localization.date_format';
+    case LOCALIZATION_DATE_TIME_FORMAT = 'localization.date_time_format';
+    case TASKS_DEFAULT_LIST_COLUMNS = 'tasks.default_list_columns';
+
+    public static function getLocales(): array
+    {
+        return [
+            'en' => 'English',
+            'es' => 'Español',
+            'pt_BR' => 'Português (Brasil)',
+        ];
+    }
+
+    public static function getTimezones(): array
+    {
+        $regions = [
+            'Africa' => DateTimeZone::AFRICA,
+            'America' => DateTimeZone::AMERICA,
+            'Antarctica' => DateTimeZone::ANTARCTICA,
+            'Aisa' => DateTimeZone::ASIA,
+            'Atlantic' => DateTimeZone::ATLANTIC,
+            'Europe' => DateTimeZone::EUROPE,
+            'Indian' => DateTimeZone::INDIAN,
+            'Pacific' => DateTimeZone::PACIFIC,
+        ];
+
+        $timezones = [];
+        foreach ($regions as $name => $mask) {
+            $zones = DateTimeZone::listIdentifiers($mask);
+            foreach ($zones as $timezone) {
+                // Lets sample the time there right now
+                $time = new DateTime(null, new DateTimeZone($timezone));
+                $utcTime = new DateTime(null, new DateTimeZone('UTC'));
+
+                // Us Americans can't handle millitary time
+                $ampm = $time->format('H') > 12 ? ' ('.$time->format('g:i a').')' : '';
+
+                $time_offset = $time->getOffset() / 3600;
+                $utc_offset = $utcTime->getOffset() / 3600;
+
+                // Remove region name and add a sample time
+                $timezones[$name][$timezone] =
+                    substr($timezone, strlen($name) + 1).' - '.
+                    $time->format('H:i').$ampm.
+                    ' ('.$time_offset - $utc_offset.'h) ';
+            }
+        }
+
+        return $timezones;
+    }
+
+    public static function getDateFormats(): array
+    {
+        return [
+            'd/m/Y' => 'd/m/Y',
+            'm/d/Y' => 'm/d/Y',
+            'Y/m/d' => 'Y/m/d',
+            'd-m-Y' => 'd-m-Y',
+            'm-d-Y' => 'm-d-Y',
+            'Y-m-d' => 'Y-m-d',
+            'd.m.Y' => 'd.m.Y',
+            'm.d.Y' => 'm.d.Y',
+            'Y.m.d' => 'Y.m.d',
+        ];
+    }
+
+    public static function getDateTimeFormats(): array
+    {
+        return [
+            'd/m/Y H:i' => 'd/m/Y H:i',
+            'd/m/Y H:i:s' => 'd/m/Y H:i:s',
+            'm/d/Y h:i' => 'm/d/Y h:i',
+            'm/d/Y h:i:s' => 'm/d/Y h:i:s',
+            'm/d/Y h:i A' => 'm/d/Y h:i A',
+            'Y/m/d H:i' => 'Y/m/d H:i',
+            'Y/m/d H:i:s' => 'Y/m/d H:i:s',
+            'd-m-Y H:i' => 'd-m-Y H:i',
+            'd-m-Y H:i:s' => 'd-m-Y H:i:s',
+            'm-d-Y h:i' => 'm-d-Y h:i',
+            'm-d-Y h:i:s' => 'm-d-Y h:i:s',
+            'm-d-Y h:i A' => 'm-d-Y h:i A',
+            'Y-m-d H:i' => 'Y-m-d H:i',
+            'Y-m-d H:i:s' => 'Y-m-d H:i:s',
+            'd.m.Y H:i' => 'd.m.Y H:i',
+            'd.m.Y H:i:s' => 'd.m.Y H:i:s',
+            'm.d.Y h:i' => 'm.d.Y h:i',
+            'm.d.Y h:i:s' => 'm.d.Y h:i:s',
+            'm.d.Y h:i A' => 'm.d.Y h:i A',
+            'Y.m.d H:i' => 'Y.m.d H:i',
+            'Y.m.d H:i:s' => 'Y.m.d H:i:s',
+        ];
+    }
+}
